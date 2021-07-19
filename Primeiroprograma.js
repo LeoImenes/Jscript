6a4b655de372ba5e1fdd3edc3bf94e03a4d9984e
LeoImenes <leonardoimenes@hotmail.com>
2021-07-19 18:29:09 -0300

    Frio

---
+++ Primeiroprograma.js
@@ -0,0 +1,15 @@
+console.log("Primeiro programa");
+
+const idade1 = 20;
+const idade2 = 18;
+console.log(idade1);
+console.log(idade2 * 2);
+
+
+const idadesoma = idade1 + idade2;
+
+console.log("A soma de", idade1, "e", idade2, "é:", idadesoma)
+
+const ola = 2.2 * 9;
+
+console.log("ola %f ", ola)
